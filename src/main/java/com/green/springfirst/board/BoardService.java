0902a3159543715daf6e24043fb8f8cf670b434e
DoheumPark <pirbak@hanmail.net>
2023-11-07 17:12:25 +0900

    first

---
+++ src/main/java/com/green/springfirst/board/BoardService.java
@@ -0,0 +1,18 @@
+package com.green.springfirst.board;
+
+import com.green.springfirst.board.model.BoardVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardMapper mapper;
+
+    public List<BoardVo> getBoard() {
+        return mapper.selBoardList();
+    }
+}
